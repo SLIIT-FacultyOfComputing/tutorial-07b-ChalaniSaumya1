b7df4f27e033b001d7d12cd9584c9a4a5d764d49
ChalaniS <99180967+ChalaniSaumya1@users.noreply.github.com>
2022-05-27 08:44:02 +0000

    I completed it as tutorial-7.

--- Item.cpp
+++ Item.cpp
@@ -3,22 +3,42 @@
 using namespace std;
 
 // 4. Implement Default Constructor Implementation
+
+Item::Item(){
+  itemCode = 0;
+  unitPrice = 0;
+  discount = 0;
+};
+
 // 5. Implement Overloaded Constructor Implementation
+
+Item::Item(int iCode, float uPrice, float d){
+  itemCode = iCode;
+  unitPrice = uPrice;
+  discount = d;
+};
+
+
 // 6. Implement Destructor (display "Destructor Called")
+
+Item::~Item{
+  std::cout << "Destructor Called"<< std::endl;
+};
+
 
 void Item::setDiscount(float pdiscount) {
   discount = pdiscount;
-}
+};
 
 float Item::getDiscount() {
   return discount;
-}
+};
 
 float Item::discountedPrice() {
    return unitPrice - unitPrice * discount/100;
-}
+};
 
 void Item::display() {
   cout << "Item : " << itemCode << endl;
   cout << "Discounted Price " << discountedPrice() << endl;
-}
+};
